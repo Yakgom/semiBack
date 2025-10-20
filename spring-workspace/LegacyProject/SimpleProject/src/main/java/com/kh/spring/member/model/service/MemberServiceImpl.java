19445931114f9670cd9c93e57e2141399e402b8f
yakgom <dirrha123@naver.com>
2025-10-21 08:43:59 +0900

    commit

--- spring-workspace/LegacyProject/SimpleProject/src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
+++ spring-workspace/LegacyProject/SimpleProject/src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.spring.exception.TooLargeValueException;
 import com.kh.spring.member.model.dao.MemberRepository;
 import com.kh.spring.member.model.dto.MemberDTO;
 
@@ -49,6 +50,23 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	public void signUp(MemberDTO member) {
+		// 꼼꼼하게 검증
+		// 유효값 검증
+		if(member==null) {
+			return;
+		}
+		
+		//아이디 값이 20자가 넘으면 안된다고 가정해보자
+		if(member.getUserId().length()>20) {
+			throw new TooLargeValueException("아이디 값이 너무 길어요");
+		}
+		if(member.getUserId()==null||
+				member.getUserId().trim().isEmpty()||
+				member.getUserPwd()==null||
+				member.getUserPwd().trim().isEmpty()) {
+			return;
+		}
+		
 	}
 
 	@Override
